a5cb420aa3cc7b27c33d924c06c6afccbd9d2603
mohyeonMan <qkrwlgns0510@gmail.com>
2022-09-19 15:57:15 +0900

    220919

--- 03_loop/src/for_/For01.java
+++ 03_loop/src/for_/For01.java
@@ -1,5 +1,22 @@
 package for_;
 
 public class For01 {
-
+	public static void main(String[] args) {
+		
+		for(int i=0; i<10;i++) {
+			System.out.println("Hello Java "+i);
+		}
+		
+		System.out.println();
+		
+		for(int j=10;j>0;j--) {
+			System.out.println("Hello Java " + j);
+		}
+		
+		System.out.println();
+		
+		for(int i='A';i<='Z';i++) {
+			System.out.print((char)(i)+" ");			
+		}
+	}	
 }
