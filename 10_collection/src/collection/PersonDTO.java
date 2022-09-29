d5d41c42ab366658975ba7ef85123b9deea3b952
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2022-09-29 16:36:14 +0900

    220929

--- 10_collection/src/collection/PersonDTO.java
+++ 10_collection/src/collection/PersonDTO.java
@@ -1,5 +1,39 @@
 package collection;
 
 public class PersonDTO {
+	private String name;
+	private int age;
+
+//-----------------------------
+	public PersonDTO() { //컨트롤+스페이스 디폴트생성자 자동으로 잡아준다
+		// TODO Auto-generated constructor stub
+	}
+	public PersonDTO(String name, int age) {
+
+		this.name = name;
+		this.age = age;
+	}
+
+//--------------------------
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "이름 + "+name+"\t나이 = "+age;
+	}
+	
 
 }
--- 10_collection/src/collection/PersonMain.java
+++ 10_collection/src/collection/PersonMain.java
@@ -1,5 +1,25 @@
 package collection;
 
+import java.util.ArrayList;
+
 public class PersonMain {
+
+	public void init(ArrayList<PersonDTO> person) {
+		PersonDTO aa = new PersonDTO("홍길동", 25);
+		PersonDTO bb = new PersonDTO("프로도", 40);
+		PersonDTO cc = new PersonDTO("죠르디", 30);
+		person.add(aa);
+		person.add(bb);
+		person.add(cc);
+	}
+
+	public static void main(String[] args) {
+		ArrayList<PersonDTO> person = new ArrayList<PersonDTO>();
+		PersonMain personMain = new PersonMain();
+		personMain.init(person);
+		for (int i = 0; i < person.size(); i++) {
+			System.out.println(person.get(i)); // toString Overriding
+		}
+	}
 
 }
