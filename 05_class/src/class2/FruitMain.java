64ec8d0ddb92464caaab48c5dcd97608252474fd
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2022-09-29 10:14:27 +0900

    220929

--- 05_class/src/class2/FruitMain.java
+++ 05_class/src/class2/FruitMain.java
@@ -3,13 +3,13 @@ package class2;
 class Fruit {
 
 	String pum;
-	int jan;
-	int feb;
-	int mar;
-	int tot;
-	static int sumJan = 0;
-	static int sumFeb = 0;
-	static int sumMar = 0;
+	private int jan;
+	private int feb;
+	private int mar;
+	private int tot;
+	private static int sumJan = 0;
+	private static int sumFeb = 0;
+	private static int sumMar = 0;
 
 	public Fruit(String pum, int jan, int feb, int mar) {
 		this.pum = pum;
@@ -20,13 +20,13 @@ class Fruit {
 
 	public void calcTot() {
 		tot = jan + feb + mar;
+		sumJan += jan;
+		sumFeb += feb;
+		sumMar += mar;
 	}
 
 	public void display() {
 		System.out.println(pum + "\t" + jan + "\t" + feb + "\t" + mar + "\t" + tot);
-		sumJan += jan;
-		sumFeb += feb;
-		sumMar += mar;
 	}
 
 	public static void output() {
@@ -36,10 +36,9 @@ class Fruit {
 
 public class FruitMain {
 	public static void main(String[] args) {
-		Fruit[] f = new Fruit[3];
-		f[0] = new Fruit("사과", 100, 80, 75);
-		f[1] = new Fruit("포도", 30, 25, 10);
-		f[2] = new Fruit("딸기", 25, 30, 90);
+		Fruit[] f = {new Fruit("사과", 100, 80, 75),
+					new Fruit("포도", 30, 25, 10),
+					new Fruit("딸기", 25, 30, 90)};
 		
 		System.out.println	("-----------------------------------"
 				+ 			"\nPUM\tJAN\tFEB\tMAR\tTOT\n"
--- 08_interface/src/sungJuk/SungJukInsert.java
+++ 08_interface/src/sungJuk/SungJukInsert.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class SungJukInsert implements SungJuk {
 
-	public void execute(ArrayList<SungJukDTO> list) {
+	public void execute(ArrayList<SungJukDTO> list) { // 자료형 식별자, 매개변수로 활용.
 		SungJukDTO sungJukDTO = new SungJukDTO();
 		list.add(sungJukDTO);
 	}
